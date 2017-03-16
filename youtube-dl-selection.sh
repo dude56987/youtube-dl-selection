5fa9c032c466d8ceaa93ea27eb6e9d1b9d01ee6e
dude56987 <dude56987@users.noreply.github.com>
2017-03-16 04:42:13 -0400

    Switched queue management system
    - Changed queue system from task-spooler to GNU parallel
    - Updated package dependencies

--- youtube-dl-selection.sh
+++ youtube-dl-selection.sh
@@ -17,8 +17,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ########################################################################################
 cd ~/Downloads;
-# task spooler limit queue to 3 items
-tsp -S 3
 # grab the clipboard selection into a variable
 selection=$(xsel -o)
 # figure out the tile of the selected video
@@ -29,23 +27,7 @@ filePath="$(youtube-dl --get-filename $selection)"
 message="Added '$title' to download queue..."
 notify-send -i folder-download-symbolic "$message" || echo "$message"
 # add task spooler task for youtube-dl-selection, set the id from the output
-id=$(tsp youtube-dl -c $selection)
-# Show all the active and queued processes
-tsp -C
-tsp -l
-# wait for the process to exit the queue
-while [ "$(tsp -s $id)" == "queued" ];do
-	# clear the screen
-	clear
-	# clear and show the queue
-	tsp -l
-	# more output for the user
-	echo # blank line to seprate out the below info
-	echo "Download of $id '$title' waiting in queue..."
-	sleep 4
-done
-# tail the output of the task to the end, this will wait till the execution is finished
-tsp -t $id
+sem --fg --retries 10 --no-notice --ungroup --jobs 3 --id downloadQueue "youtube-dl -c $selection"
 # check if the file was successfully downloaded by checking if the file exists
 # now this works because youtube-dl generates .part files during the download
 if [[ -f "$HOME/Downloads/$filePath" ]];then
@@ -58,6 +40,4 @@ else
 fi
 # send the properly formatted return message with the correct icon
 notify-send -i $icon "$message" || dialog --msgbox "$message" 5 21 || echo "$message"
-# clear the queue of finished items, this should include this item
-tsp -C
 ########################################################################################
