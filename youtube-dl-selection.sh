1256635508856e9b9606c045add84460176dbbf6
dude56987 <dude56987@users.noreply.github.com>
2017-05-28 05:01:14 -0400

    Updates
    - Added a user configurable download location at ~/.youtube-dl-selection
    - Added a check to make sure the URL is using HTTP or HTTPS
    - Added forced wait time between web requests to prevent disconnects
    - CLI messages are now displayed on the terminal prior to trying libnotify
      and dialog
    - Added a log in the download directory to list URLs that failed to download

--- youtube-dl-selection.sh
+++ youtube-dl-selection.sh
@@ -16,28 +16,71 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ########################################################################################
-cd ~/Downloads;
+echo "Checking for download configuration at '$HOME/.youtube-dl-selection'"
+# check for a user defined download path
+if [ -f ~/.youtube-dl-selection ];then
+	# load the config file
+	downloadPath=$(cat ~/.youtube-dl-selection)
+else
+	# if no config exists create the default config
+	downloadPath="$HOME/Downloads"
+	# write the new config from the path variable
+	echo "$downloadPath" > ~/.youtube-dl-selection
+fi
+echo "Download path set to '$downloadPath'"
+# create the path if it does not exist, then move into it
+mkdir -p $downloadPath
+cd $downloadPath
 # grab the clipboard selection into a variable
 selection=$(xsel -o)
+# if the url is not a valid web address exit
+echo "Scanning the selection for video links..."
+if ! echo "$selection" | grep "http";then
+	# build and display the error message to the user
+	message="The selection '$selection' is not a valid url, youtube-dl-selection will now exit!"
+	icon="computer-fail-symbolic"
+	echo "$message"
+	notify-send -i $icon "$message" || dialog --infobox "$message" 5 21
+	# add failure message to the log
+	echo "$message" | sed "s/, youtube-dl-selection will now exit!/.../g" >> $downloadPath/youtube-dl_FAILED.log
+	exit
+fi
+# sleeping between website pulls with youtube-dl pervents being blocked or
+# disconnected, saying processing is a lie but will probably cause less
+# confusion
+echo "Processing title..."
+sleep 5
 # figure out the tile of the selected video
 title=$(youtube-dl --get-title $selection)
+echo "Processing filename..."
+sleep 5
 # get the file path and escape the white spaces
-filePath="$(youtube-dl --get-filename $selection)"
+fileName="$(youtube-dl --get-filename $selection)"
 # send a notification
 message="Added '$title' to download queue..."
-notify-send -i folder-download-symbolic "$message" || echo "$message"
+echo "$message"
+notify-send -i folder-download-symbolic "$message" || dialog --infobox "$message" 5 21
 # add task spooler task for youtube-dl-selection, set the id from the output
-sem --fg --retries 10 --no-notice --ungroup --jobs 3 --id downloadQueue "youtube-dl -c $selection"
+# sleep for 15 seconds before download starts, this delays between failures, and delays after the above youtube-dl uses
+sem --fg --retries 10 --no-notice --ungroup --jobs 3 --id downloadQueue "echo 'Processing...';sleep 15;youtube-dl -c $selection"
 # check if the file was successfully downloaded by checking if the file exists
 # now this works because youtube-dl generates .part files during the download
-if [[ -f "$HOME/Downloads/$filePath" ]];then
+if [[ -f "$downloadPath/$fileName" ]];then
 	# set the return message to success or failure, use a success icon or failure icon in notification
 	message="SUCCESS: Download of '$title' complete!"
 	icon="checkbox-checked-symbolic"
 else
+	# add the header to the error log only once
+	header="You can attempt to finish the download using the given URL."
+	if ! grep "$header" $downloadPath/youtube-dl_FAILED.log;then
+		echo "$header" >> $downloadPath/youtube-dl_FAILED.log
+	fi
+	# add failed download links to a text file
+	echo "Failed to download '$title' at '$selection'" >> $downloadPath/youtube-dl_FAILED.log
 	message="FAILURE: Download of '$title' failed!"
 	icon="computer-fail-symbolic"
 fi
 # send the properly formatted return message with the correct icon
-notify-send -i $icon "$message" || dialog --msgbox "$message" 5 21 || echo "$message"
+echo "$message"
+notify-send -i $icon "$message" || dialog --infobox "$message" 5 21
 ########################################################################################
